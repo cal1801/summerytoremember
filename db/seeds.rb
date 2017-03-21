fd1b2423f7861ff541af771d25920f6140d55726
cal1801 <pike1801@gmail.com>
2017-03-21 10:56:18 -0500

    Add seed information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'contacts.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Contact.new
+  t.f_name = row['f_name']
+  t.l_name = row['l_name']
+  t.email = row['email']
+  t.save
+
+  camp = Camp.find_by_name row['camp']
+  camp.update_column('contact_id', t.id)
+  puts "#{t.f_name}, #{t.l_name} saved for #{camp.name}"
+end
+
+puts "There are now #{Contact.count} rows in the contacts table"
